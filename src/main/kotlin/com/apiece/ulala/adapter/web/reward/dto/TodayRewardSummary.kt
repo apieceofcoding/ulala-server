c06129b71950d51456672ae8aad8ea2d7de79a42
sanserif28 <sanserif28@gmail.com>
2025-11-09 02:17:30 +0900

    data class 제거

--- src/main/kotlin/com/apiece/ulala/adapter/web/reward/dto/TodayRewardSummary.kt
+++ src/main/kotlin/com/apiece/ulala/adapter/web/reward/dto/TodayRewardSummary.kt
@@ -2,7 +2,7 @@ package com.apiece.ulala.adapter.web.reward.dto
 
 import java.math.BigDecimal
 
-data class TodayRewardSummary(
+class TodayRewardSummary(
     val totalPoint: BigDecimal,
     val totalExp: BigDecimal
 )
